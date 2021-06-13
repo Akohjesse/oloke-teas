99b9559e9f590e6ee3b67da85d6cf684b26569b4
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-06-13 14:06:44 +0100

    animating the landing page

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+
 document.querySelector('.menu_btn .bars i').addEventListener('click', ()=>{
 	document.querySelector('.menu_box').style = 'display:block;';
 })
@@ -18,3 +19,39 @@ const swiper = new Swiper('.swiper-container', {
 	disableOnInteraction: false,
 	},
 });
+
+const LandingPageText = {
+	Header: {
+		head: ['Olókè','Locally made'],
+		Under: ['Premium Teas' ,'To serve you']
+	},
+	TextUnder: ['Olókè Tea is not only concerned about business success but ethics and sustainability. That is why all products are locally sourced, cruelty free and tested to ensure that only the best is served to the millions who choose the brand daily.', 'Nigerian tea company, founded by Tejúmólá Abiólá-Olókè, a tea enthusiast. Since the year 2000, she and her team of tea lovers have experimented with several teas from across the globe and have discovered the farm fresh taste of premium tea sourced from the highest Plateaus in Nigeria. ']
+}
+
+
+animateLanding();
+
+function animateLanding(){
+	var hh = LandingPageText.Header.head;
+	var hu = LandingPageText.Header.Under;
+	var t = LandingPageText.TextUnder;
+	var hhitem = document.querySelector('.text-bg span');
+	var huitem = document.querySelector('.text-bg h1');
+	var titem = document.querySelector('.text-bg p')
+	var x = 0;
+	var l = 1;
+	setInterval(()=>{
+		 hhitem.innerHTML = hh[x];
+		 huitem.innerHTML = hu[x];
+		 titem.innerHTML = t[l];
+		 l--;
+		x++;
+		if (x >= LandingPageText.Header.head.length){
+			x= 0;
+		}
+		if(l < 0){
+			l=1;
+		}
+	   
+	 }, 5000)
+}
